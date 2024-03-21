d28972b3f213723e0e1f01bbb65f8ba9d27cb03f
MUJAHID <116384473+mujahidshaik28@users.noreply.github.com>
2024-03-21 14:07:04 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo yum install git -y
 sudo apt install python3-pip -y
 git clone https://github.com/mujahidshaik28/fishdetails.git
 cd fishdetails
 pip3 install -r requirements.txt
-screem -m -d python3 app.py
+screen -m -d python3 app.py
